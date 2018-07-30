1429df4208950e3cfc0d2ffac0f4f6032b66d9ec
ranegaurish <update1989>
2018-07-30 11:42:54 +0300

    package update

--- admin-src2/src/app/shared/helper.service.ts
+++ admin-src2/src/app/shared/helper.service.ts
@@ -1,8 +1,8 @@
-import {Router} from '@angular/router';
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {HttpParamsOptions} from "@angular/common/http/src/params";
-import {AuthService} from "./auth/auth.service";
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { HttpParamsOptions } from "@angular/common/http/src/params";
+import { AuthService } from "./auth/auth.service";
 
 declare const Tour: any;
 declare const Noty: any;
@@ -179,8 +179,8 @@ export class Utils {
 @Injectable()
 export class HttpHelper {
     // baseUrl: string = 'http://192.168.2.150:81/assets-mg-portal/eapi/';
-    // baseUrl: string = 'http://assetalbum.com/eapi/';
-    baseUrl: string = 'http://localhost/assets-mg-portal/eapi/';
+    baseUrl: string = 'http://assetalbum.com/eapi/';
+    // baseUrl: string = 'http://localhost/assets-mg-portal/eapi/';
 
     defaultOptions = {
         headers: {
@@ -193,7 +193,7 @@ export class HttpHelper {
     }
 
     post(url: string, data?, options?) {
-        options = {...this.defaultOptions, ...options};
+        options = { ...this.defaultOptions, ...options };
         data = {
             ...data, ...{
                 'token': this.authService.getToken()
