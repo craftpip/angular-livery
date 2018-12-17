fe9c4491cfeeff19ebaa2243a9046bdbf53c08f3
Boniface Pereira <boniface.pereira@itshastra.com>
2018-12-17 17:03:21 +0530

    add exception doc for $e in index,

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
  */
 /** allow response only from angular 5 response */
 //header('Access-Control-Allow-Origin: *', false);
- header('Access-Control-Allow-Origin: http://localhost:4200', false);
+header('Access-Control-Allow-Origin: http://localhost:4203', false);
 //header('Access-Control-Allow-Origin: http://88.201.64.182:800', false);
 header('Access-Control-Allow-Credentials: true', false);
 
@@ -78,6 +78,7 @@ $routerequest = function ($route = null, $e = false) {
             ->response();
     }
     else {
+        /** @var Exception $e */
         throw $e;
     }
 
