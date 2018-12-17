1d3528bf841a8fbb577d529837b2f9171df61fbf
Boniface Pereira <boniface.pereira@itshastra.com>
2018-12-17 15:58:06 +0530

    update

--- admin-src/src/app/layouts/full/full-layout.component.ts
+++ admin-src/src/app/layouts/full/full-layout.component.ts
@@ -9,20 +9,9 @@ import {AppEvents} from "../../shared/helper.service";
 })
 
 export class FullLayoutComponent {
-
-    yo: string = 'help';
-
     constructor(
         public events: AppEvents,
     ) {
-        let id = this.events.on('hey', (data) => {
-            alert(JSON.stringify(data));
-        });
 
-        this.events.off(id);
-
-        this.events.on('hey', (data) => {
-            alert('Wot' + JSON.stringify(data));
-        });
     }
 }
